23f67f514a3a6259a0e975237e710d1497a686c3
ATariq1 <aliumar18@gmail.com>
2021-02-05 00:01:33 -0800

    Fixed backwards wifi pins, the RFS worked!

--- CPEN391_Computer/synthesis/submodules/CPEN391_Computer_SysID.v
+++ CPEN391_Computer/synthesis/submodules/CPEN391_Computer_SysID.v
@@ -49,7 +49,7 @@ module CPEN391_Computer_SysID (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1612467708 : 0;
+  assign readdata = address ? 1612505041 : 0;
 
 endmodule
 
